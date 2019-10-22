2f99443490ee0e0a9dfc0189e7e6d669da301987
Jeremy Watrelos <jeremy.watrelos@gmail.com>
2019-10-22 16:27:41 +0200

    (minor-update) : Add new supported fields in table

--- src/components/GameTable.vue
+++ src/components/GameTable.vue
@@ -18,11 +18,16 @@
             class="elevation-1">
 
       <template v-slot:item.image="{ item }">
-          <img v-bind:src="'data:image/jpeg;base64,'+item.image" @click="downloadGame(item)"/>
+          <img v-bind:src="'data:image/jpeg;base64,'+item.image"/>
       </template>
 
       <template v-slot:item.score="{ item }">
         <v-chip :color="getColor(item.score)" dark>{{ item.score }}</v-chip>
+      </template>
+
+      <template v-slot:item.superXCI="{ item }">
+        <v-icon color="green" v-if='item.superXCI'>fa-check</v-icon>
+        <v-icon color="red" v-else>fa-times</v-icon>
       </template>
 
       <template v-slot:item.canBeDownloaded="{ item }">
@@ -52,12 +57,14 @@
           { text: '', value: 'image', sortable: false },
           { text: 'Title', align: 'left', value: 'title'},
           { text: 'Size (Mo)', value: 'size' },
+          { text: 'SuperXCI', value: 'superXCI' },
+          { text: 'Nb DLC', value: 'nbDLC' },
+          { text: 'Version', value: 'version' },
           { text: 'Score', value: 'score' },
           { text: 'Release Date', value: 'releaseDate' },
           { text: 'Genre', value: 'genre' },
           { text: 'Developer', value: 'developer' },
           { text: 'Rating', value: 'rating' },
-          { text: 'Extension', value: 'extension' },
           { text: '', value: 'canBeDownloaded' }
         ],
         info: [],
